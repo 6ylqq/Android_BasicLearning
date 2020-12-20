3fef37036e12ffc20f4555c9553628657b534223
6ylqq <937192804@qq.com>
2020-12-20 22:28:08 +0800

    增加了Channel，不然Android8.0以上无法显示通知，但貌似通知不能在开机启动的时候就显示，会提示一个bug，目前没解决。不过实验要求的是点击按钮出现即可

--- app/src/main/java/com/example/experiment_1/broadcast/BootBroadcastReceiver.java
+++ app/src/main/java/com/example/experiment_1/broadcast/BootBroadcastReceiver.java
@@ -1,32 +1,37 @@
 package com.example.experiment_1.broadcast;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.example.experiment_1.R;
 
+/**
+ * @author ylqq
+ */
 public class BootBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String CHANNEL_ID = "BootBroadcastReceiverID";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        //绑定manager，通知栏属于系统服务
-        NotificationManager manager= (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        //通知栏配置
-        Notification notification=new Notification.Builder(context)
-                .setContentTitle("title")
-                .setContentText("hello world")
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),R.mipmap.ic_launcher))
+        String textTitle = "BootBroadcastReceiver";
+        String textContent = "开机启动的通知";
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .build();
-
-        //显示通知栏
-        manager.notify(1,notification);
+        NotificationChannel notificationChannel = new NotificationChannel("AppTestNotificationId", "AppTestNotificationName", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.createNotificationChannel(notificationChannel);
+        notificationManager.notify(1,notification);
     }
 }
