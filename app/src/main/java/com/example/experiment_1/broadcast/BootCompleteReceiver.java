d0afbfb8b602d3c4e0cbd53d5da0e18208eaf35d
6ylqq <937192804@qq.com>
2020-12-20 22:29:52 +0800

    再次打印了信息，开机启动显示的Toast不稳定，有时能出现，有时不能出现，以后解决

--- app/src/main/java/com/example/experiment_1/broadcast/BootCompleteReceiver.java
+++ app/src/main/java/com/example/experiment_1/broadcast/BootCompleteReceiver.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+/**
+ * @author ylqq
+ */
 public class BootCompleteReceiver extends BroadcastReceiver {
     private static final String TAG = "BootCompleteReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG,"开机启动MsgService...");
+        System.out.println("开机启动MsgService...");
+        //TODO 开机显示的Toast不稳定，有时候能显示有时候不能显示
         Toast.makeText(context,"开机启动",Toast.LENGTH_LONG).show();
     }
 }
